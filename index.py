1ebefd8b84ff0ae6db1906fb6dd4b8e6f38e7070
Anthony ADELAIDE <anthony.adelaide@orange.fr>
2021-12-27 23:05:02 +0100

    Initial commit

---
+++ index.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+import cgi
+
+form = cgi.FieldStorage()
+
+password = "password"
+
+print("Content-type: text/html; charset=utf-8\n")
+
+
+def header(title):
+    pass
+
+
+def footer():
+    print("""
+        </BODY>
+        </HTML>
+    """)
+
+
+html = """
+<HTML>
+<HEAD><TITLE>Page de connexion</TITLE></HEAD>
+<BODY>
+<CENTER>
+<FORM method="POST" action="index.py">
+<paragraph> Entrez votre nom d'utilisateur : <input type="text" name="login">
+<paragraph> Entrez votre mot de passe : <input type="password" name="password">
+<paragraph> <input type="submit" value="Connect">
+</FORM>
+</CENTER>
+<HR>
+
+</BODY>
+</HTML>
+"""
+print(html)
+
+
+if form["login"].value != "" and form["password"].value == password:
+    print("<center><hr><H3>Welcome back,", form["login"].value, ".</H3><hr></center>")
+
+else:
+    print("<H1>Erreur</H1>")
+    print("Veuillez rentrer votre nom d'utilisateur et mot de passe svp")
+
